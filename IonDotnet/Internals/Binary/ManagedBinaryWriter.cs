6fb1fac99d555d454c5ad35114a756b21d567ff3
Rickard Lyrenius <rly@combination.se>
2018-09-03 16:26:38 +0200

    Changed the behavior of the binary reader/writer to conform to the spec, see http://amzn.github.io/ion-docs/docs/symbols.html#examples "In this case, the first local symbol table generated only needs to add one new local symbol for the top-level value being serialized in its context. The second symbol table adds a subsequent symbol to the context for the value immediately following it. This pattern of local symbol tables allows top-level values to be written to a stream without knowing all symbols ahead of time"

--- IonDotnet/Internals/Binary/ManagedBinaryWriter.cs
+++ IonDotnet/Internals/Binary/ManagedBinaryWriter.cs
@@ -24,7 +24,8 @@ namespace IonDotnet.Internals.Binary
             SystemSymbols,
             LocalSymbolsWithImportsOnly,
             LocalSymbols,
-            LocalSymbolsFlushed
+            LocalSymbolsFlushed,
+            SubsequentLocalSymbols
         }
 
         private class ImportedSymbolsContext
@@ -109,7 +110,6 @@ namespace IonDotnet.Internals.Binary
             var lengthSegment = new List<Memory<byte>>(2);
             _symbolsWriter = new RawBinaryWriter(lengthWriterBuffer, new PagedWriter256Buffer(), lengthSegment);
             _userWriter = new RawBinaryWriter(lengthWriterBuffer, new PagedWriter256Buffer(), lengthSegment);
-
             _importContext = new ImportedSymbolsContext(importedTables);
             _locals = new Dictionary<string, int>();
         }
@@ -121,7 +121,17 @@ namespace IonDotnet.Internals.Binary
         /// <param name="writeIvm">Whether to write the Ion version marker</param>
         private void StartLocalSymbolTableIfNeeded(bool writeIvm)
         {
-            if (_symbolState != SymbolState.SystemSymbols) return;
+            if (_symbolState == SymbolState.SubsequentLocalSymbols)
+            {
+                _symbolsWriter.AddTypeAnnotationSymbol(Symbols.GetSystemSymbol(SystemSymbols.IonSymbolTableSid));
+                _symbolsWriter.StepIn(IonType.Struct);
+                _symbolState = SymbolState.LocalSymbolsWithImportsOnly;
+                return;
+            }
+            else if (_symbolState != SymbolState.SystemSymbols)
+            {
+                return;
+            }
 
             if (writeIvm)
             {
@@ -352,9 +362,7 @@ namespace IonDotnet.Internals.Binary
             _userWriter.Finish();
 
             //reset local symbols
-            _locals.Clear();
-            _localsLocked = false;
-            _symbolState = SymbolState.SystemSymbols;
+            _symbolState = SymbolState.SubsequentLocalSymbols;
         }
 
         public override void SetFieldName(string name)
